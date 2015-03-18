2742f970c89188daff30a42584a2c95c1b03107f
Crostantin <crostantin@gmail.com>
2015-03-18 15:20:12 +0100

    Let's start
    - main: ro.secure=0
    - main: enable_target_debugging := true
    - ota_release_tools: comment out build date assert
    - core: PRODUCT_BUILD_PROP_OVERRIDES
    - allow override of device asserts
    - core: remove vendor_module_check.mk
    - core: drop the check-product-copy-files
    - telephony: default dataroaming=false
    
    thanks to @Rascarlo
    
    Conflicts:
    	core/main.mk
    	core/product_config.mk
    	core/tasks/vendor_module_check.mk
    	tools/releasetools/edify_generator.py
    	tools/releasetools/ota_from_target_files

--- core/main.mk
+++ core/main.mk
@@ -315,24 +315,25 @@ enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
     tags_to_install += debug
   else
     # Disable debugging in plain user builds.
-    enable_target_debugging :=
+    enable_target_debugging := true
   endif
 
   # Turn on Dalvik preoptimization for user builds, but only if not
   # explicitly disabled and the build is running on Linux (since host
   # Dalvik isn't built for non-Linux hosts).
   ifeq (,$(WITH_DEXPREOPT))
-    ifeq ($(user_variant),user)
-      ifeq ($(HOST_OS),linux)
-        # TODO: turn on WITH_DEXPREOPT for libart user builds.
-        # WITH_DEXPREOPT := true
+    ifeq ($(DALVIK_VM_LIB),libdvm.so)
+      ifeq ($(user_variant),user)
+        ifeq ($(HOST_OS),linux)
+          WITH_DEXPREOPT := false
+        endif
       endif
     endif
   endif
